383de439b71b5cb263b3d1182da38b11cde8b25a
Stephen Vajda <svajda@rymaxinc.com>
2019-03-21 08:55:05 -0400

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
-{//http://localhost:5000/values/5
+{   [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -20,6 +21,8 @@ namespace DatingApp.API.Controllers
         }
         // GET api/values
         [HttpGet]
+        [Authorize]
+        [AllowAnonymous]
         public async Task<IActionResult> GetValues ()
         {
              var values= await _context.Values.ToListAsync();
@@ -29,6 +32,7 @@ namespace DatingApp.API.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
+        [Authorize]
        public async Task<IActionResult> GetValue(int id)
         {
              var value=  await _context.Values.FirstOrDefaultAsync(x => x.Id==id);
@@ -38,18 +42,21 @@ namespace DatingApp.API.Controllers
 
         // POST api/values
         [HttpPost]
+        [Authorize]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
+        [Authorize]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
+        [Authorize]
         public void Delete(int id)
         {
         }
