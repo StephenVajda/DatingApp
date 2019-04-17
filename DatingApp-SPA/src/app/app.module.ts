a3805bfa069461ed5db850b491a020d12bda91f6
Stephen Vajda <svajda@rymaxinc.com>
2019-04-17 08:03:32 -0400

    End of section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+
 
 
 @NgModule({
@@ -20,11 +25,12 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
-   ],
-   providers: [AuthService, ErrorInterceptorProvider],
+      FormsModule,
+      BsDropdownModule.forRoot(),
+    ],
+   providers: [AuthService, AlertifyService, ErrorInterceptorProvider],
    bootstrap: [
       AppComponent
    ]
 })
-export class AppModule {}
+export class AppModule { }
