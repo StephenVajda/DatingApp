537b0d946b74660fcf75275d6af230d51e1102d3
Stephen Vajda <sgv@rockwoodpl.com>
2019-06-24 08:49:14 -0400

    End of section 10

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -19,6 +19,7 @@ export class AuthService {
         if (user) {
           localStorage.setItem('token', user.token);
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log('vajda token', this.decodedToken)
           
         }
       })
--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { idLocale } from 'ngx-bootstrap';
+import { AlertifyService } from './alertify.service';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
- 
+
   baseUrl = environment.apiUrl;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private alertify: AlertifyService) { }
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.baseUrl + 'users');
   }
@@ -17,5 +20,10 @@ export class UserService {
 
     return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
+
+  updateUser(id: number , user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
+  }
+
 
 }
